eb340ab025c3b6d18dce29f6208d973df6ab3b0f
Amariah Robinson <arobin95@email.cpcc.edu>
2021-03-09 15:12:42 -0500

    Revert "Changes to Hello World"
    This reverts commit 263ad11834f152e441bc7a89c8d77d16672aea82.

--- Hello World.java
+++ Hello World.java
@@ -4,9 +4,8 @@
 
 public class HelloWorld
 {
-   public static void main(String[] args)
+   public static void(main String[] args))
    {
       System.out.println("Hello World!");
-      System.out.println("Program Modified, but able to revert to originial using GitHub Desktop.");
    }
 }
