d73c61c37a4be05132b6701125270fe89e246e1d
Amariah Robinson <arobin95@email.cpcc.edu>
2021-03-09 14:55:05 -0500

    Create Hello World.java

---
+++ Hello World.java
@@ -0,0 +1,11 @@
+// Amariah Robinson
+// 03.09.21
+// Git Hub Repositories Practice
+
+public class HelloWorld
+{
+   public static void(main String[] args))
+   {
+      System.out.println("Hello World!");
+   }
+}
